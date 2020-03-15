d73dd03ca1927ca87b9d80f4ee03b1a0266a6df9
adachi hideo <higan.ibn5100@gmail.com>
2020-03-16 01:26:12 +0900

    mc

--- gui2.py
+++ gui2.py
@@ -1,8 +1,17 @@
 import PySimpleGUI as sg
 from PIL import Image
+import io
+
+# img = Image.open("~/ubuntu/homework/img_pros3/Hyperspectral_to_rgb_image/Encastre 10_1cm.PNG")
+img = Image.open("Encastre 10_1cm.PNG")
+bio = io.BytesIO()
+img.save(bio, format="PNG")
+img = bio.getvalue()
+# return bio.getvalue()
 
 layout = [
-    [sg.Image("LyteProLPF2.PNG")]
+    # [sg.Image(img.tobytes("raw"))]
+    [sg.Image(data=img)]
 ]
 
 window = sg.Window('住所を入力', layout)
