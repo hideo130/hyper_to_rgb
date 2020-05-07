aa92217f2d90215ecd8a09fb013aa97fea96e5b4
Adachi Hideo <h.adachi.769@nitech.jp>
2020-05-07 18:10:33 +0900

    ひとまず完成

---
+++ get_sd_fig.py
@@ -0,0 +1,25 @@
+from colour import SpectralDistribution
+import numpy as np
+import colour
+
+
+def load_light_distribution(name="lamp_spectrum.csv"):
+    sd_light_source = np.loadtxt(name, skiprows=1, dtype="float")
+    sd_light_source = sd_light_source[np.where(sd_light_source[:, 0] >= 400)]
+    return sd_light_source
+
+
+def get_sd_fig(disp_name, load_name, axes=None):
+    """
+
+    fig, axes  = colour.plotting.plot_single_sd(sd, standalone=False)
+    """
+    sd_light_source = load_light_distribution(name=load_name)
+    # print(sd_light_source[:5,0])
+    sd = SpectralDistribution(sd_light_source[:, 1], domain=sd_light_source[:, 0],  name=disp_name)
+    # sd = SpectralDistribution(sd_light_source, name='Sample')
+    # print(type(fig), type(axes))
+    if axes:
+        return colour.plotting.plot_single_sd(sd, cmfs='CIE 1964 10 Degree Standard Observer', axes=axes, standalone=False)
+    else:
+        return colour.plotting.plot_single_sd(sd, cmfs='CIE 1964 10 Degree Standard Observer', standalone=False)
--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import io
 import numpy as np
 from skimage.external.tifffile import imread
 from tiff_to_rgb import tiff_to_rgb
+from get_sd_fig import get_sd_fig
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib
+import japanize_matplotlib
+from pathlib import Path
+matplotlib.use('Agg')
 
 
 def call_img(img):
@@ -17,31 +23,47 @@ def call_img(img):
     return img
 
 
-himg = imread("data2.tiff")
+def draw_figure(canvas, figure, loc=(0, 0)):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
+
+
+himg = imread("./data/data.tiff")
 himg = himg / 2**16
+himg = himg[::2, ::2, :]
 img = Image.fromarray(np.uint8(himg[:, :, 0]*255))
 img = call_img(img)
+disp_name_to_load_name = {"標準光源D65": "./csvs/D65.csv", '標準光源A': "./csvs/A.csv",
+                          "光源A": "./csvs/Pictures.csv", "光源B": "./csvs/LightScience.csv"}
+load_name = "./csvs/D65.csv"
+figure, axes = get_sd_fig(disp_name="標準光源D65", load_name=load_name)
+figure_x, figure_y, figure_w, figure_h = figure.bbox.bounds
 
-# img = Image.open("A.PNG")
 
 layout = [
-    # [sg.Image(img.tobytes("raw"))]
-    # [sg.Button('Check'), sg.Quit()],
-    [sg.Quit()],
-    [sg.T("Show 1 channel.", key="_SHOW_", size=(25, 1), font=('Helvetica 20')),
-     sg.Slider(range=(0, 60), orientation='h',enable_events=True,
-               size=(34, 20), key='__SLIDER__', default_value=0)
-     ],
-    [sg.Button("Rendering")],
-    [sg.Image(data=img,  key='_OUTPUT_')],
+    [sg.Frame("Menu", [
+        [sg.T("400 nm ~ 410 nmを表示", key="_SHOW_",
+              size=(25, 1), font=('Helvetica 20'))],
+        [sg.Slider(range=(0, 60), orientation='h', enable_events=True,
+                   size=(34, 20), key='__SLIDER__', default_value=0)],
+        [sg.Combo(('標準光源D65', '標準光源A', "光源A", "光源B"), key="__DIST__", default_value='標準光源D65',
+                  enable_events=True, size=(20, 1)), sg.Button("Rendering"), sg.Quit()],
+        [sg.Canvas(size=(figure_w, figure_h), key='canvas')]
+    ]),
+        sg.Image(data=img,  key='_OUTPUT_')],
 
 ]
 
-window = sg.Window('sRGBをレンダリング', layout)
+
+window = sg.Window('sRGBをレンダリング', layout, finalize=True)
+fig_canvas_agg = draw_figure(window['canvas'].TKCanvas, figure)
+
 
 while True:
     event, values = window.read()
-    print(event)
+    # print(event)
     if event in (None, 'Quit'):
         print('exit')
         break
@@ -51,12 +73,25 @@ while True:
         img = Image.fromarray(np.uint8(himg[:, :, tmp]*255))
         img = call_img(img)
         window['_OUTPUT_'].update(data=img)
-        window["_SHOW_"].update("Show %d channel." % (int(tmp)))
-        print("update img")
+        window["_SHOW_"].update("%d nm ~ %d nmを表示" % (400+10*tmp, 400+10*(tmp+1)))
+        # print("update img")
     elif event == "Rendering":
-        img = tiff_to_rgb(himg)
+        img = tiff_to_rgb(himg, load_name)
+        display_name = values["__DIST__"]
+        # img.save("results/%s_k=1.png" % (display_name))
         img = call_img(img)
         window['_OUTPUT_'].update(data=img)
-        window["_SHOW_"].update("Show sRGB image.")
+        window["_SHOW_"].update("レンダリング画像を表示")
 
+    elif event == "__DIST__":
+        display_name = values["__DIST__"]
+        load_name = disp_name_to_load_name[display_name]
+        print(load_name)
+        img = tiff_to_rgb(himg, load_name)
+        img = call_img(img)
+        window['_OUTPUT_'].update(data=img)
+        window["_SHOW_"].update("レンダリング画像を表示")
+        axes.cla()
+        figure, axes = get_sd_fig(display_name, load_name=load_name, axes=axes)
+        fig_canvas_agg.draw()
 window.close()
